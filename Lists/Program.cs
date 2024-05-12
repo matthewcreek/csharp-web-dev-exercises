6e693a7b1da9107655fa00a0cb3ed90919cedbf8
Matthew Creek <matthew.creek27@gmail.com>
2024-05-12 00:38:09 -0500

    lists

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -2,4 +2,28 @@
 
 //TODO: Write your code for List Practice below:
 
-Console.WriteLine("Hello, Lists!");
+static double AddNums(List<double> nums){
+    double sum = 0;
+    foreach (double num in nums){
+        sum += num;
+    };
+    return sum;
+};
+
+List<double> numList = new() { 1,2,3,4,5,6,7,8,9,10};
+
+System.Console.WriteLine(AddNums(numList));
+
+static void PrintStrings(List<string> strings) {
+    System.Console.WriteLine("How many letters do you seek? ");
+    int wordLength = int.Parse(Console.ReadLine());
+    foreach (string word in strings) {
+        if (word.Length == wordLength) {
+            System.Console.WriteLine(word);
+        }
+    }
+}
+
+List<string> wordList = new() {"hamburger", "clone", "evil", "apothecary", "xi", "cat"};
+
+PrintStrings(wordList);
