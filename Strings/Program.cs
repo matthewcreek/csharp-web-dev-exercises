2c7af66ba3794409af987bd60655fd487e6d237c
Matthew Creek <matthew.creek27@gmail.com>
2024-05-12 00:22:55 -0500

    string

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -2,5 +2,9 @@
 
 //TODO: Write your code for String Practice below:
 
-Console.WriteLine("Hello, Strings!");
+string sentence = @"I would not, could not, in a box. I would not, could not with a fox.
+I will not eat them in a house. I will not eat them with a mouse.";
+
+string[] sentenceArray = sentence.Split(".");
+System.Console.WriteLine(string.Join(",", sentenceArray));
 
