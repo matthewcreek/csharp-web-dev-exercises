0f51d29b2a453674395c2e071b7de33609ab96f2
Matthew Creek <matthew.creek27@gmail.com>
2024-05-12 23:06:11 -0500

    dictionaries

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -1,6 +1,28 @@
-﻿//Exercises: Control Flow & Collections
+﻿// //Exercises: Control Flow & Collections
 
-//TODO: Write your code for Dictionary Practice below:
+// //TODO: Write your code for Dictionary Practice below:
 
-Console.WriteLine("Hello, Dictionary!");
+Dictionary<int, string> students = new Dictionary<int, string>();
+string newStudent;
 
+System.Console.WriteLine("Enter your students (or ENTER to finish):");
+do 
+{
+    System.Console.WriteLine("Student: ");
+    newStudent = Console.ReadLine();
+
+    if (newStudent != "") 
+    {
+        System.Console.WriteLine("ID Number: ");
+        int newID = int.Parse(Console.ReadLine());
+        students.Add(newID, newStudent);
+    };  
+} 
+while (newStudent != "");
+    
+System.Console.WriteLine("\nClass Roster");
+foreach (KeyValuePair<int, string> student in students) 
+{
+    System.Console.WriteLine("Student name: " + student.Value + "\tID: " + student.Key);
+}
+System.Console.WriteLine("Total students: " + students.Count);
