4ce403f5200ca3355e1291895fe2f0a0d4d7e854
Matthew Creek <matthew.creek27@gmail.com>
2024-05-21 14:34:41 -0500

    bonus mission complete

--- IceCreamShop/FlavorComparer.cs
+++ IceCreamShop/FlavorComparer.cs
@@ -6,7 +6,17 @@ namespace IceCreamShop
     {
         public int Compare(Flavor x, Flavor y)
         {
-            return string.Compare(x.Name, y.Name);
+            double diff = x.Allergens.Count - y.Allergens.Count;
+            if (diff == 0)
+            {
+                return 0;
+            }
+            else if (diff > 0)
+            {
+                return 1;
+            }
+            else return -1;
+            // return string.Compare(x.Name, y.Name);
         }
     }
 }
