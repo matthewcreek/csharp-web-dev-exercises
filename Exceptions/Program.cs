c5b8612533b64b44f04c19992e901b34853be4ce
Matthew Creek <matthew.creek27@gmail.com>
2024-05-29 19:53:11 -0500

    added project to solution file

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,13 +1,23 @@
 ﻿// --------- Divide By Zero! ----------
 
-/* 
+
 static double Divide(double x, double y)
 {
     // Write your code here!
+    if (y.Equals(0))
+    {
+        throw new ArgumentOutOfRangeException("y", "Cannot divide by zero!");
+    }
+    else
+    {
+        return x/y;
+    }
 }
-*/
+
 
 // Test out your Divide() function here!
+Console.WriteLine(Divide(6,3));
+Console.WriteLine(Divide(6,0));
 
 
 // --------- Test Student Labs ----------
