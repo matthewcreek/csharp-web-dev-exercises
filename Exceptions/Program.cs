dd4342e66b1ae15b6f3d883689141073933cf858
Matthew Creek <matthew.creek27@gmail.com>
2024-05-29 20:10:57 -0500

    exercises chapter 11 complete

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -16,25 +16,55 @@ static double Divide(double x, double y)
 
 
 // Test out your Divide() function here!
-Console.WriteLine(Divide(6,3));
-Console.WriteLine(Divide(6,0));
+try
+{
+    Console.WriteLine(Divide(6,3));
+    Console.WriteLine(Divide(6,0));
+}
+catch (ArgumentOutOfRangeException e)
+{
+    
+    Console.WriteLine(e.Message);
+}
 
 
 // --------- Test Student Labs ----------
 
-/*
+
 static int CheckFileExtension(string fileName)
 {
-    // Write your code here!
+    if (fileName.Equals(null) || fileName.Equals(""))
+    {
+        throw new NullReferenceException("Filename cannot be blank.");
+    }
+    else if (fileName.Contains(".cs"))
+    {
+        return 1;
+    }
+    else return 0;
 }
-*/
+
 
 // Test out your CheckFileExtension() function here!
 
-Dictionary<string, string> students = new Dictionary<string, string>();
-students.Add("Carl", "Program.cs");
-students.Add("Brad", "");
-students.Add("Elizabeth", "MyCode.cs");
+Dictionary<string, string> students = new()
+{
+    { "Carl", "Program.cs" },
+    { "Brad", "" },
+    { "Elizabeth", "MyCode.cs" }
+};
 
+foreach (KeyValuePair<string,string> student in students)
+{
+    try
+    {
+        Console.WriteLine($"{student.Key}: {CheckFileExtension(student.Value)}");   
+    }
+    catch (NullReferenceException e)
+    {
+        
+        Console.WriteLine($"{student.Key}: {e.Message}");
+    }
+}
 
 
